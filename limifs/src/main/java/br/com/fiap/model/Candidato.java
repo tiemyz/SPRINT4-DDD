a577fb27a9c6b4945a3a6bdcfd5c2100f73a7bd8
tiemyz <rm95145@fiap.com.br>
2022-10-11 19:56:25 -0300

    model candidato com informações inseridas.

---
+++ limifs/src/main/java/br/com/fiap/model/Candidato.java
@@ -0,0 +1,73 @@
+package br.com.fiap.model;
+
+public class Candidato {
+
+    private String nomeCandidato;
+    private String telCandidato;
+    private String dataCandidato;
+    private String cpfCandidato;
+    private String generoCandidato;
+    private String senhaCandidato;
+    private String emailCandidato;
+
+    public Candidato (String nomeCandidato, String telCandidato, String dataCandidato, String cpfCandidato, String generoCandidato, String senhaCandidato, String emailCandidato) {
+
+        this.nomeCandidato = nomeCandidato;
+        this.telCandidato = telCandidato;
+        this.dataCandidato = dataCandidato;
+        this.cpfCandidato = cpfCandidato;
+        this.generoCandidato = generoCandidato;
+        this.senhaCandidato = senhaCandidato;
+        this.emailCandidato = emailCandidato;
+
+    }
+    
+    public String getNomeCandidato() {
+        return nomeCandidato;
+    }
+    public void setNomeCandidato(String nomeCandidato) {
+        this.nomeCandidato = nomeCandidato;
+    }
+
+    public String getTelCandidato() {
+        return telCandidato;
+    }
+    public void setTelCandidato(String telCandidato) {
+        this.telCandidato = telCandidato;
+    }
+
+    public String getDataCandidato() {
+        return dataCandidato;
+    }
+    public void setDataCandidato(String dataCandidato) {
+        this.dataCandidato = dataCandidato;
+    }
+    
+    public String getCpfCandidato() {
+        return cpfCandidato;
+    }
+    public void setCpfCandidato(String cpfCandidato) {
+        this.cpfCandidato = cpfCandidato;
+    }
+
+    public String getGeneroCandidato() {
+        return generoCandidato;
+    }
+    public void setGeneroCandidato(String generoCandidato) {
+        this.generoCandidato = generoCandidato;
+    }
+
+    public String getSenhaCandidato() {
+        return senhaCandidato;
+    }
+    public void setSenhaCandidato(String senhaCandidato) {
+        this.senhaCandidato = senhaCandidato;
+    }
+
+    public String getEmailCandidato() {
+        return emailCandidato;
+    }
+    public void setEmailCandidato(String emailCandidato) {
+        this.emailCandidato = emailCandidato;
+    }
+}
