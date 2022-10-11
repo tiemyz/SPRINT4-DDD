aeaf681bac0f77e8f628c8e1b4c3b1aa5d4a0058
tiemyz <rm95145@fiap.com.br>
2022-10-11 19:56:57 -0300

    arquivos para serem atualizados.

--- limifs/src/main/java/br/com/fiap/controller/PrimaryController.java
+++ limifs/src/main/java/br/com/fiap/controller/PrimaryController.java
@@ -1,4 +1,4 @@
-package br.com.fiap;
+package br.com.fiap.controller;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
---
+++ limifs/src/main/java/br/com/fiap/dao/CandidatoDAO.java
@@ -0,0 +1,35 @@
+package br.com.fiap.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+
+import br.com.fiap.model.Candidato;
+
+public class CandidatoDAO {
+
+
+
+    public void cadastrar(Candidato candidato) throws SQLException {
+
+        Connection con = ConnectionFactory.getConnection();
+        var stm = con.prepareStatement(
+            "INSERT INTO T_LIMIFS_CANDIDATO (nom_candidato, tel_candidato, data_candidato, cpf_candidato, genero_candidato, senha_candidato, email_candidato) " +
+            "VALUES (?,?,?,?,?,?,?)"
+            );
+
+        stm.setString(1,candidato.getNomeCandidato());
+        stm.setString(2,candidato.getTelCandidato());
+        stm.setString(3,candidato.getDataCandidato());
+        stm.setString(4,candidato.getCpfCandidato());
+        stm.setString(5,candidato.getGeneroCandidato());
+        stm.setString(6,candidato.getSenhaCandidato());
+        stm.setString(7,candidato.getEmailCandidato());
+
+
+        stm.execute();
+        con.close();
+    }
+}
+
