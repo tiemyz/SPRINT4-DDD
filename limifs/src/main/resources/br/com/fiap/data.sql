e2bfd5340de306d9d0fce9a8f457f2d5e93c61d8
tiemyz <rm95145@fiap.com.br>
2022-10-11 19:55:38 -0300

    fxmls e data.sql atualizados.

---
+++ limifs/src/main/resources/br/com/fiap/data.sql
@@ -0,0 +1,12 @@
+CREATE TABLE T_LIMIFS_CANDIDATO (
+    
+    nom_candidato       VARCHAR NOT NULL, 
+    tel_candidato       VARCHAR NOT NULL, 
+    data_candidato      DATE NOT NULL, 
+    cpf_candidato       VARCHAR NOT NULL, 
+    genero_candidato    VARCHAR NOT NULL, 
+    senha_candidato     VARCHAR NOT NULL, 
+    email_candidato     VARCHAR NOT NULL
+);
+
+
