92bfadf08991417c4700bf3508755632c5a99623
tiemyz <rm95145@fiap.com.br>
2022-10-12 16:54:28 -0300

    tabelas criadas.

--- limifs/target/classes/br/com/fiap/data.sql
+++ limifs/target/classes/br/com/fiap/data.sql
@@ -1,12 +1,35 @@
 CREATE TABLE T_LIMIFS_CANDIDATO (
     
-    nom_candidato       VARCHAR NOT NULL, 
-    tel_candidato       VARCHAR NOT NULL, 
-    data_candidato      DATE NOT NULL, 
-    cpf_candidato       VARCHAR NOT NULL, 
-    genero_candidato    VARCHAR NOT NULL, 
-    senha_candidato     VARCHAR NOT NULL, 
-    email_candidato     VARCHAR NOT NULL
+    nom_candidato       VARCHAR2 (50)   NOT NULL, 
+    tel_candidato       VARCHAR2 (20)   NOT NULL, 
+    data_candidato      DATE            NOT NULL, 
+    cpf_candidato       VARCHAR2 (15)   NOT NULL, 
+    genero_candidato    VARCHAR2 (20)   NOT NULL, 
+    senha_candidato     VARCHAR2 (200)  NOT NULL, 
+    email_candidato     VARCHAR2 (200)  NOT NULL
 );
 
+SELECT * FROM T_LIMIFS_CANDIDATO;
 
+CREATE TABLE T_LIMIFS_EMPRESA (
+
+    nom_empresa       VARCHAR2 (50)   NOT NULL, 
+    loc_empresa       VARCHAR2 (20)   NOT NULL, 
+    tipo_empresa      VARCHAR2 (15)   NOT NULL, 
+    email_empresa     VARCHAR2 (20)   NOT NULL, 
+    cnpj_empresa      VARCHAR2 (200)  NOT NULL, 
+    senha_empresa     VARCHAR2 (200)  NOT NULL
+);
+
+SELECT * FROM T_LIMIFS_EMPRESA;
+
+CREATE TABLE T_LIMIFS_CURRICULO (
+
+    grau_curriculo    VARCHAR2 (70)   NOT NULL,
+    cursos_curriculo  VARCHAR2 (400)  NOT NULL,
+    certi_curriculo   VARCHAR2 (400)  NOT NULL,
+    exp_curriculo     VARCHAR2 (400)  NOT NULL
+
+);
+
+SELECT * FROM T_LIMIFS_CURRICULO;
