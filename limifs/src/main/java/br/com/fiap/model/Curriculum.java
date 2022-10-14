99b4d1c5f7a1f75e7636412e235eeef672e7acad
tiemyz <rm95145@fiap.com.br>
2022-10-13 22:36:29 -0300

    models atualizados.

--- limifs/src/main/java/br/com/fiap/model/Curriculum.java
+++ limifs/src/main/java/br/com/fiap/model/Curriculum.java
@@ -1,5 +1,45 @@
 package br.com.fiap.model;
 
 public class Curriculum {
+
+    private String grauCurriculo;
+    private String cursosCurriculo;
+    private String certCurriculo;
+    private String expCurriculo;
+
+    public Curriculum (String grauCurriculo, String cursosCurriculo, String certCurriculo, String expCurriculo) {
+
+        this.grauCurriculo = grauCurriculo;
+        this.cursosCurriculo = cursosCurriculo;
+        this.certCurriculo = certCurriculo;
+        this.expCurriculo = expCurriculo;
+    }
     
+    public String getGrauCurriculo() {
+        return grauCurriculo;
+    }
+    public void setGrauCurriculo(String grauCurriculo) {
+        this.grauCurriculo = grauCurriculo;
+    }
+
+    public String getCursosCurriculo() {
+        return cursosCurriculo;
+    }
+    public void setCursosCurriculo(String cursosCurriculo) {
+        this.cursosCurriculo = cursosCurriculo;
+    }
+
+    public String getCertCurriculo() {
+        return certCurriculo;
+    }
+    public void setCertCurriculo(String certCurriculo) {
+        this.certCurriculo = certCurriculo;
+    }
+
+    public String getExpCurriculo() {
+        return expCurriculo;
+    }
+    public void setExpCurriculo(String expCurriculo) {
+        this.expCurriculo = expCurriculo;
+    }
 }
--- limifs/src/main/java/br/com/fiap/model/Empresas.java
+++ limifs/src/main/java/br/com/fiap/model/Empresas.java
@@ -1,5 +1,64 @@
 package br.com.fiap.model;
 
 public class Empresas {
+
+    private String nomeEmpresas;
+    private String locEmpresas;
+    private String tipoEmpresas;
+    private String emailEmpresas;
+    private String cnpjEmpresas;
+    private String senhaEmpresas;
+
+    public Empresas (String nomeEmpresas, String locEmpresas, String tipoEmpresas, String emailEmpresas, String cnpjEmpresas, String senhaEmpresas) {
+
+        this.nomeEmpresas = nomeEmpresas;
+        this.locEmpresas  = locEmpresas;
+        this.tipoEmpresas = tipoEmpresas;
+        this.emailEmpresas = emailEmpresas;
+        this.cnpjEmpresas = cnpjEmpresas;
+        this.senhaEmpresas = senhaEmpresas;
+
+    }
+
+    public String getNomeEmpresas() {
+        return nomeEmpresas;
+    }
+    public void setNomeEmpresas(String nomeEmpresas) {
+        this.nomeEmpresas = nomeEmpresas;
+    }
+
+    public String getLocEmpresas() {
+        return locEmpresas;
+    }
+    public void setLocEmpresas(String locEmpresas) {
+        this.locEmpresas = locEmpresas;
+    }
+
+    public String getTipoEmpresas() {
+        return tipoEmpresas;
+    }
+    public void setTipoEmpresas(String tipoEmpresas) {
+        this.tipoEmpresas = tipoEmpresas;
+    }
+
+    public String getEmailEmpresas() {
+        return emailEmpresas;
+    }
+    public void setEmailEmpresas(String emailEmpresas) {
+        this.emailEmpresas = emailEmpresas;
+    }
+
+    public String getCnpjEmpresas() {
+        return cnpjEmpresas;
+    }
+    public void setCnpjEmpresas(String cnpjEmpresas) {
+        this.cnpjEmpresas = cnpjEmpresas;
+    }
     
+    public String getSenhaEmpresas() {
+        return senhaEmpresas;
+    }
+    public void setSenhaEmpresas(String senhaEmpresas) {
+        this.senhaEmpresas = senhaEmpresas;
+    }
 }
