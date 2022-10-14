261662f6a6b6b0c4026eb08b2ba73877e5bb3161
tiemyz <rm95145@fiap.com.br>
2022-10-14 10:48:12 -0300

    module atualizado.

--- limifs/src/main/java/module-info.java
+++ limifs/src/main/java/module-info.java
@@ -3,6 +3,6 @@ module br.com.fiap {
     requires javafx.fxml;
     requires java.sql;
 
-    opens br.com.fiap to javafx.fxml;
+    opens br.com.fiap.controller to javafx.fxml;
     exports br.com.fiap;
 }
